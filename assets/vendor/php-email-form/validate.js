0754da6380764d00278263869ad495b3daaa9e8a
aswin <aswinachuz894@gmail.com>
2023-01-17 11:39:22 +0530

    acv

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,38 +1,39 @@
 function formsubmission(){
+   var subject=document.Submission.subject.value;
+  var message=document.Submission.message.value; 
   var name=document.Submission.name.value;
   var email=document.Submission.email.value;
-  var subject=document.Submission.subject.value;
-  var message=document.Submission.message.value; 
+  
   var regex=/^([a-zA-Z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/gm;
- var err=document.getElementById('error-message');
+  var err=document.getElementById('error');
   
  if(name==""){
-     err.innerHTML='The Name is Empty'
+     err.innerHTML='please enter name'
      
      return false;
 }
 if(name.length<5){
- err.innerHTML='Name Should Contain five Elements Please Include Fullname';
+ err.innerHTML='Name Should Contain five Elements ';
      return false;
 }
 if(email=="")
 {
- err.innerHTML='Email is Empty';
+ err.innerHTML='please enter email';
      return false;
 }  
 if(regex.test(email)==false){
  err.innerHTML='Invalid Email'
  return false
 }
-if(subject.length<5){
- err.innerHTML='Should Contain 5 Elements';
+if(subject.length<6){
+ err.innerHTML='Should Contain 6 Elements';
  return false
 } 
 if(message.length<10){
  err.innerHTML='Should Contain 10 Words';
  return false
 } 
-if(message.length>100){
+if(message.length>200){
  err.innerHTML='Exceeds Limit';
  return false
 } 
