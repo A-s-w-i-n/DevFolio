40aae31c14a48e268b4ea4eaea8fddb4e026f068
aswin <aswinachuz894@gmail.com>
2023-01-17 12:10:45 +0530

    adf

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -3,40 +3,58 @@ function formsubmission(){
   var message=document.Submission.message.value; 
   var name=document.Submission.name.value;
   var email=document.Submission.email.value;
-  
+
   var regex=/^([a-zA-Z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/gm;
   var err=document.getElementById('error');
   
+
  if(name==""){
      err.innerHTML='please enter name'
      
      return false;
+
 }
 if(name.length<5){
  err.innerHTML='Name Should Contain five Elements ';
      return false;
 }
+
+
 if(email=="")
 {
  err.innerHTML='please enter email';
      return false;
 }  
+
+
 if(regex.test(email)==false){
  err.innerHTML='Invalid Email'
  return false
 }
+
 if(subject.length<6){
- err.innerHTML='Should Contain 6 Elements';
+ err.innerHTML='Should Contain 6 character';
  return false
 } 
-if(message.length<10){
- err.innerHTML='Should Contain 10 Words';
+
+if(subject.length>15){
+    err.innerHTML='limit exceed';
+    return false
+}
+
+
+if(message.length<20){
+ err.innerHTML='Should Contain 20 character';
  return false
 } 
+
+
 if(message.length>200){
  err.innerHTML='Exceeds Limit';
  return false
 } 
+
+
 return true;
 
 }
